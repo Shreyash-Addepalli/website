cd02061f8835718c63701ebda5781e968c23756e
Shreyash A <100358889+Shreyash-Addepalli@users.noreply.github.com>
2022-12-17 16:23:39 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,25 +6,25 @@ const Footer = () => {
   return (
     <section className="footer">
       <div className="social">
-        <a href="">
+        <a href="https://www.instagram.com/de_plandao">
           <FaInstagram />
         </a>
-        <a href="">
+        <a href="https://twitter.com/de_planDAO">
           <FaTwitter />
         </a>
-        <a href="">
+        <a href="https://t.me/akshay93aditya">
           <FaTelegram />
         </a>
       </div>
       <ul className="list">
         <li>
-          <a href="">Join Us</a>
+          <a href="https://airtable.com/shrcRaQxpWF0RofzC">Join Us</a>
         </li>
         <li>
-          <a href="">Workspace</a>
+          <a href="https://de-plan.notion.site/de_plan-Workspace-7513a7123f5f4a46adba6d2476de2287">Workspace</a>
         </li>
         <li>
-          <a href="">Contact Us</a>
+          <a href="mailto:teamde.plan@gmail.com?cc=akshay93aditya@gmail.com,shreyash.addepalli@gmail.com">Contact Us</a>
         </li>
       </ul>
       <p className="copyright">de_plan@2022</p>
