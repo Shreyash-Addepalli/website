97d1b2cf30245eb3717e833de3283ba080f6f0e3
Shreyash A <100358889+Shreyash-Addepalli@users.noreply.github.com>
2022-12-20 19:23:51 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Footer.css";
-import { FaDiscord, FaInstagram, FaTwitter, FaTelegram } from "react-icons/fa";
+import {  FaInstagram, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -12,19 +12,27 @@ const Footer = () => {
         <a href="https://twitter.com/de_planDAO">
           <FaTwitter />
         </a>
-        <a href="https://t.me/akshay93aditya">
-          <FaTelegram />
-        </a>
+//         <a href="https://t.me/akshay93aditya">
+//           <FaTelegram />
+//         </a>
       </div>
       <ul className="list">
         <li>
-          <a href="https://airtable.com/shrcRaQxpWF0RofzC">Join Us</a>
+          <a onClick={() => {
+              window.open(
+                "https://airtable.com/shrcRaQxpWF0RofzC"
+              );
+            }}>Join Us</a>
         </li>
         <li>
-          <a href="https://de-plan.notion.site/de_plan-Workspace-7513a7123f5f4a46adba6d2476de2287">Workspace</a>
+          <a onClick={() => {
+              window.open(
+                "https://de-plan.notion.site/de_plan-Workspace-7513a7123f5f4a46adba6d2476de2287"
+              );
+            }}>Workspace</a>
         </li>
         <li>
-          <a href="mailto:teamde.plan@gmail.com?cc=akshay93aditya@gmail.com,shreyash.addepalli@gmail.com">Contact Us</a>
+          <a href="mailto:team@de-plan.xyz?cc=shreyash@de-plan.xyz">Contact Us</a>
         </li>
       </ul>
       <p className="copyright">de_plan@2022</p>
